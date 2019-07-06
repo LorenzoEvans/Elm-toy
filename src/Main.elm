116859ef270a4d00fe4514f961cc99e0e9ea430b
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-07-05 23:45:48 -0400

    Working on model/view. so quick.

--- src/Main.elm
+++ src/Main.elm
@@ -1,5 +1,23 @@
 module Main exposing (main)
 
-import Html exposing (text)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
 
-main = text "hi"
+
+main : Program Never Model Msg
+main =
+    Html.program
+        { init = init
+        , update = update
+        , subscriptions = \_ -> Sub.none
+        , view = view
+        }
+
+type alias Model =
+    { quote : String
+    }
+
+init : (Model, Cmd Msg)
+init =
+    ( Model "", Cmd.none)
