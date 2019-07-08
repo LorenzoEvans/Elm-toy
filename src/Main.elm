c01b58e28644d002f8a8eda469bae7c715f7fdac
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-07-07 20:29:57 -0400

    Writing small counter app.

--- src/Main.elm
+++ src/Main.elm
@@ -3,3 +3,13 @@ import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
+
+type alias Model =
+    { count : Int }
+
+init: Model
+init =
+    {count = 0}
+
+
+main : Program () Model Msg
