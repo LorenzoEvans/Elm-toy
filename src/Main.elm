9869f62986c964605cba11fd54b8423c5f3302b8
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-07-07 20:36:54 -0400

    Working on view.

--- src/Main.elm
+++ src/Main.elm
@@ -11,5 +11,19 @@ init: Model
 init =
     {count = 0}
 
+type Msg = Inc | Dec | Reset
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Inc ->
+            { model | count = model.count + 1}
+        Dec ->
+            { model | count = model.count - 1}
+        Reset ->
+            { model | count = 0}
+
+view : Model -> Html Msg
+view =
 
 main : Program () Model Msg
