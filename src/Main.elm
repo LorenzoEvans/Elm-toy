9b8d4ddbe350bdf327fb9a1c13f57db36c60a3bd
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-07-07 21:32:29 -0400

    *This* version of the code works, no idea why.

--- src/Main.elm
+++ src/Main.elm
@@ -1,41 +1,50 @@
 module Main exposing (main)
+
 import Browser
-import Html exposing (..)
-import Html.Attributes exposing (..)
-import Html.Events exposing (..)
+import Html exposing (Html, button, div, text)
+import Html.Events exposing (onClick, onInput)
+
 
 type alias Model =
     { count : Int }
 
-initialModel : Model
 
+initialModel : Model
 initialModel =
-    {count = 0}
+    { count = 0 }
+
 
 type Msg = Inc | Dec | Reset
+
 
 update : Msg -> Model -> Model
 update msg model =
     case msg of
         Inc ->
-            { model | count = model.count + 1}
+            { model | count = model.count + 1 }
+
         Dec ->
-            { model | count = model.count - 1}
+            { model | count = model.count - 1 }
+
         Reset ->
-            { model | count = 0}
+            { model | count = 0 }
+
 
 view : Model -> Html Msg
-view =
+view model =
     div []
-    [ button [ onClick Inc ] [ text "Increment Value."]
-    , div [] [ text <| String.fromInt initialModel.count]
-    , button [ onClick Dec ] [ text "Decrement Value."]
-    , button [ onClick Reset ] [ text "Reset Value."]
-    ]
+        [ button [ onClick Inc ] [ text "+1" ]
+        , div [] [ text <| String.fromInt model.count ]
+        , button [ onClick Dec ] [ text "-1" ]
+        , button [ onClick Reset ] [ text "Reset"]
+        ]
+
 
 main : Program () Model Msg
 main =
     Browser.sandbox
         { init = initialModel
         , view = view
-        , update = update}
+        , update = update
+        }
+
