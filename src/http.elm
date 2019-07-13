5f1cb814c8c8a6412d3b44700c2c1c04b8dd6a8f
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-07-12 20:03:48 -0400

    Adding deps.

--- src/http.elm
+++ src/http.elm
@@ -2,8 +2,14 @@ import Browser
 import Html exposing (Html, text, pre)
 import Http
 
+
+
 main =
     Browser.element
-        {
-
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
         }
+
+type Model = Failure | Loading | Success String
