c0cba25f96bf94f828036c46f868db4903c6cf2d
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-07-12 21:23:28 -0400

    formatting/error handling.

--- src/http.elm
+++ src/http.elm
@@ -1,43 +1,80 @@
+module Main exposing (ApiResult(..), Model(..), Msg(..), init, main, subscriptions, update, view)
+
 import Browser
-import Html exposing (Html, text, pre)
+import Html exposing (Html, pre, text)
 import Http
 
-type Model = Failure | Loading | Success String
-type Msg =
-    GotText (Result Http.Error String)
 
-update : Msg -> Model -> (Model, Cmd Msg)
+type Model
+    = Failure
+    | Loading
+    | Success String
+
+
+type Msg
+    = GotText (Result Http.Error String)
+
+
+type ApiResult error value
+    = Ok value
+    | Err error
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         GotText result ->
             case result of
                 Ok fullText ->
-                    (Success fullText, Cmd.none)
+                    ( Success fullText, Cmd.none )
+
                 Err _ ->
-                    (Failure, Cmd.none)
+                    ( Failure, Cmd.none )
+
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
     Sub.none
+
 
 view : Model -> Html Msg
 view model =
     case model of
         Failure ->
             text "I was unable to find your book."
+
         Loading ->
             text "Loading.."
+
         Success fullText ->
             pre [] [ text fullText ]
 
 
+init : () -> ( Model, Cmd Msg )
 
-init : () -> (Model, Cmd Msg)
+
+
+-- In addition to our initial model, we have a command we want to execute immediately,
+-- that will eventually produce a Msg that will be fed to our update function.
+
+
 init _ =
     ( Loading
     , Http.get
-        {url = "https://elm-lang.org/assets/public-opinion.txt"
-        , expect = Http.expectString GotText})
+        --    Http.get exposes the get request function(?), or action.
+        { url = "https://elm-lang.org/assets/public-opinion.txt"
+
+        --    We define a url variable with the string of the url we want.
+        , expect = Http.expectString GotText
+        }
+    )
+
+
+
+--    We then define what we expect the data to be, and we also say that when we get this response,
+--      we want it to be turn into a GotText message.
+
+
 main =
     Browser.element
         { init = init
@@ -45,5 +82,3 @@ main =
         , update = update
         , subscriptions = subscriptions
         }
-
-
